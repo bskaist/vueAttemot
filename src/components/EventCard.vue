d1e5acbc90aadbf8d0a418fbe15da282bcb0b08f
binyamin <bskaist@student.touro.edu>
2020-07-21 15:35:26 -0400

    added prefences EventCard

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 @mouseover="mousedOver" :style="{ color: color }">Hello World!</h1>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  data() {
+    return {
+      color: 'black'
+    }
+  },
+  methods: {
+    mousedOver: function() {
+      this.color = 'red'
+    }
+  }
+}
+</script>
+
+<style></style>
